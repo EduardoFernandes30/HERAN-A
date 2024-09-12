982f427d5d29c1e975e52353e09e05b55479157e
Eduardo Fernandes Silva <164395347+EduardoFernandes30@users.noreply.github.com>
2024-09-12 17:30:42 -0300

    Add files via upload

---
+++ Banco/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Especial conta = new Especial();
+        conta.Consultar();
+
+        double ReduzirLimite = conta.DiminuirLimite(50);
+        Console.WriteLine("O limite após ser reduzido é: {0}", ReduzirLimite);
+        Console.ReadLine();
+
+        double AumentarLimite = conta.AumentarLimite(50);
+        Console.WriteLine("O limite após ser aumentado é: {0}", AumentarLimite);
+        Console.ReadLine();
+
+        Corrente banco = new Corrente();
+        banco.ConsultarSaldo();
+
+        double DepositarSaldo = banco.Depositar(50);
+        Console.WriteLine("O saldo depositado é: {0}", DepositarSaldo);
+        Console.ReadLine();
+
+        double SacarSaldo = banco.Sacar(50);
+        Console.WriteLine("O saque foi de: {0}", SacarSaldo);
+        Console.ReadLine();
+
+
+    }
+}
---
+++ Banco/contaEspecial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Especial : Corrente
+{
+    private double Limite = 1000;
+
+    public double limite { get => Limite; set => Limite = value; }
+
+    public void Consultar()
+    {
+        Console.WriteLine("O limite é: {0}", limite);
+        Console.ReadLine();
+    }
+
+    public double DiminuirLimite(double valor)
+    {
+        double LimiteDiminuido = Limite - valor;
+        return LimiteDiminuido;
+    }
+
+    public double AumentarLimite(double valor)
+    {
+        double LimiteAumentado = Limite + valor;
+        return LimiteAumentado;
+    }
+}
